07657d3be9f8aa8139020141fd62d6394dead2e2
ZhangChunXian <1836891291@qq.com>
2020-10-11 19:15:41 +0800

    complete float_i2f function in bits.c '

--- Freshman/CSAPP_in_ECNU/lab1_datalabr/datalab-handout/bits.c
+++ Freshman/CSAPP_in_ECNU/lab1_datalabr/datalab-handout/bits.c
@@ -309,11 +309,11 @@ int ilog2(int x) {
  *   Rating: 2
  */
 unsigned float_neg(unsigned uf){
-    int mask = 0x00ffffff;
 
-    if( (~ (uf << 1)) < mask) return uf;
+    if( (~ (uf << 1)) < 0x00ffffff) return uf;
     else return uf ^ (1 << 31);
 }
+
 /*
  * float_i2f - Return bit-level equivalent of expression (float) x
  *   Result is returned as unsigned int, but
@@ -324,9 +324,34 @@ unsigned float_neg(unsigned uf){
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
-  return 2;
+
+  int sign = x >> 31 & 1;
+  int i, exponent, fraction, delta, mask;
+
+  if(x == 0)  return 0;
+  if( x == 0x80000000) exponent = 158;
+  else{
+    if (sign) x = -x; // turn negative to positive
+
+    while( !(x >> i)) i--;// find out how many valid bits does x have
+
+    exponent = i + 127;     // IEEE standard
+    x = x << (31 - i);      // clear the higher bits zero
+
+    fraction = (0x7fffff) & (x >> 8);
+    x = x & 0xff;
+    delta = x> 128 || ((x == 128) && (fraction & 1)); // if lowest 8 bits of x is larger than a half, round up 1
+
+    if((fraction + delta) >> 23) {
+      fraction = (fraction + delta) & 0x7fffff;
+      exponent += 1;
+    }
+  }
+
+  return (sign << 31) | (exponent << 23) | fraction;
+
 }
-/* 
+/*
  * float_twice - Return bit-level equivalent of expression 2*f for
  *   floating point argument f.
  *   Both the argument and result are passed as unsigned int's, but
